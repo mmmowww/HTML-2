76791736feefd9b77e4f8ce43e481f5fb6f27986
MMMOWWW <36041208+mmmowww@users.noreply.github.com>
2018-04-29 20:24:11 +0300

    Add files via upload

---
+++ 5/reg.php
@@ -0,0 +1,33 @@
+<!DOCTYPE>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Arbatov Viacheslav</title>
+<link href="CSS.css" rel="stylesheet" type="text/css"/>
+</head>
+<body class= "osnova" link="green" vlink="yelow" alink="black">
+<h1>Добро пожаловать в магазин мистических товаров</h1>
+<div class ="menu"><div class ="logo"><a href = "index.html"><img src ="content/logo.jpg" width = "60px" height="50px"></img></a></div>
+<div class="link" class ="">
+<a href="index.html" title="Главная">Главная</a>
+<a href="content.html" title="Тавары" >Продукцыя</a>
+<a href="contacts.html" title="Контакты" >Контакты</a>
+<a href="reg.html" title="Регистрацыя" target = "_blank">Регистрацыя/Вход</a>
+</div>
+</div>
+<form method="GET" action="reg.php" >
+Ваше Имя <input type="text" placeholder="Иван"><br>
+Пароль <input type = "password"><br>
+Имейл<input type = "email" placeholder = "email@emil.ru" class ="email"><br>
+Год рождения <input type="date"><br>
+Ваш пол<br>
+М<input type ="radio" name ="pol" cheked><br>
+Ж<input type ="radio" name ="pol" cheked><br>
+<input type ="submit"><br>
+<input type="reset"><br>
+
+</form>
+</div>
+<footer class = "footer"> <p align="right" class="p1">Все права защещены &copy; </p></footer>
+</body>
+</html>
